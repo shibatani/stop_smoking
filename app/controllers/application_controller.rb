98ad31795021533224cb2d49fc2198c402132a5f
shibatani <rto1728@gmail.com>
2020-07-27 22:51:07 +0900

    add top_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-  # ログイン済ユーザーのみにアクセスを許可する
-  before_action :authenticate_user!
-
   # deviseコントローラーにストロングパラメータを追加する          
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
 
   protected
   def configure_permitted_parameters
