7de35540c8324ec5dd554f9648f77861f8374211
shibatani <rto1728@gmail.com>
2020-08-20 16:22:02 +0900

    ロジックの整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,35 @@ class User < ApplicationRecord
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
+
+  def continue_days
+    created_date = self.created_at.to_date 
+    today = Date.current 
+    continue_days = (today - created_date).to_i 
+    return continue_days
+  end
+
+  def frequency(continue_days)
+    frequency = continue_days / self.days
+    return frequency
+  end
+
+  def saved_money(frequency) 
+    saved_money = self.boxes * 500 * frequency
+    return saved_money
+  end
+
+  def cigarettes(frequency)
+    cigarettes = self.boxes * 20 * frequency
+    return cigarettes
+  end
+
+  def lifespan(cigarettes)
+    lifespan = cigarettes * 5
+    return lifespan
+  end
+
+  def smoking_profile_present?
+    @user.days.present? && @user.boxes.present?
+  end
 end
