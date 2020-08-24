57e098a03875a3600b3f2f907535f34a0aceb425
shibatani <rto1728@gmail.com>
2020-08-24 13:10:23 +0900

    ゲストユーザーでログインできない問題解消

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    binding.pry
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,8 @@ class User < ApplicationRecord
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "ゲストユーザー"
+      user.days = 1
+      user.boxes = 1
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
   end
